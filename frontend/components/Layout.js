a34c1507ee87628d63c1cba10499b3ff28ef081a
Mirabror Sodikov <mirabror9545@gmail.com>
2021-07-06 00:01:36 +0500

    Layout, Footer, Header and 404

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,16 +1,25 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
-import styles from "../styles/Layout.module.css";
+import styles from "@/styles/Layout.module.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Showcase from "@/components/Showcase";
 
-export default function Layout({ title, keywords, description, children }) {
+export default function Layout({title, keywords, description, children}) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description}/>
+        <meta name="keywords" content={keywords}/>
       </Head>
+      <Header/>
+      {router.pathname === "/" && <Showcase/>}
       <div className={styles.container}>{children}</div>
+      <Footer/>
     </div>
   );
 }
