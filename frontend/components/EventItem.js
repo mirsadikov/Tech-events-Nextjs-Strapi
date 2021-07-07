36258731bb9801d68a098ca71b851433f441231d
Mirabror Sodikov <mirabror9545@gmail.com>
2021-07-07 12:25:36 +0500

    Events api

--- frontend/components/EventItem.js
+++ frontend/components/EventItem.js
@@ -6,11 +6,12 @@ export default function EventItem({evt}) {
   return (
     <div className={styles.event}>
       <div className={styles.img}>
-        <Image src={evt.image ? evt.image : "/images/event-default.png"} width={170} height={100}/>
+        <Image src={evt.image ? evt.image.formats.thumbnail.url : "/ images/event-default.png"} width={170}
+               height={100}/>
       </div>
       <div className={styles.info}>
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString("uz-UZ")} at {evt.time}
         </span>
         <h3>{evt.name}</h3>
       </div>
